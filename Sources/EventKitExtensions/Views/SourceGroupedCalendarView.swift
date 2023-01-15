bd7e49fe76044abbffac07b43512343bbea8af6a
Paul Traylor <kungfudiscomonkey@gmail.com>
2023-01-15 14:19:19 +0900

    Move Today view to be a header on our Calendar view

--- Sources/EventKitExtensions/Views/SourceGroupedCalendarView.swift
+++ Sources/EventKitExtensions/Views/SourceGroupedCalendarView.swift
@@ -13,19 +13,30 @@ struct CalendarGroup {
     var calendars: [EKCalendar]
 }
 
-public struct SourceGroupedCalendarView<ContentView: View>: View {
-    private var groups: [CalendarGroup]
-    private var content: (EKCalendar) -> ContentView
+public struct SourceGroupedCalendarView<Header: View, Footer: View, ContentView: View>: View {
+    private let groups: [CalendarGroup]
+    private let content: (EKCalendar) -> ContentView
 
-    public init(groups: [EKCalendar], content: @escaping (EKCalendar) -> ContentView) {
+    private let header: Header?
+    private let footer: Footer?
+
+    public init(
+        groups: [EKCalendar],
+        @ViewBuilder header: @escaping () -> Header,
+        @ViewBuilder footer: @escaping () -> Footer,
+        @ViewBuilder content: @escaping (EKCalendar) -> ContentView
+    ) {
         let grouped = Dictionary.init(grouping: groups) { $0.source!.title }
         let mapped = grouped.map { CalendarGroup(source: $0, calendars: $1) }
         self.groups = mapped.sorted { $0.source > $1.source }
+        self.header = header()
+        self.footer = footer()
         self.content = content
     }
 
     public var body: some View {
         List {
+            self.header
             ForEach(groups, id: \.source) { group in
                 Section(group.source) {
                     ForEach(group.calendars.sorted(by: \.title)) { calendar in
@@ -33,7 +44,48 @@ public struct SourceGroupedCalendarView<ContentView: View>: View {
                     }
                 }
             }
+            self.footer
         }
         .listStyle(GroupedListStyle())
+    }
+}
+
+extension SourceGroupedCalendarView {
+    public init(
+        groups: [EKCalendar],
+        @ViewBuilder footer: @escaping () -> Footer,
+        @ViewBuilder content: @escaping (EKCalendar) -> ContentView
+    ) where Header == EmptyView {
+        self.init(
+            groups: groups,
+            header: { EmptyView() },
+            footer: footer,
+            content: content
+        )
+    }
+
+    public init(
+        groups: [EKCalendar],
+        @ViewBuilder header: @escaping () -> Header,
+        @ViewBuilder content: @escaping (EKCalendar) -> ContentView
+    ) where Footer == EmptyView {
+        self.init(
+            groups: groups,
+            header: header,
+            footer: { EmptyView() },
+            content: content
+        )
+    }
+
+    public init(
+        groups: [EKCalendar],
+        @ViewBuilder content: @escaping (EKCalendar) -> ContentView
+    ) where Header == EmptyView, Footer == EmptyView {
+        self.init(
+            groups: groups,
+            header: { EmptyView() },
+            footer: { EmptyView() },
+            content: content
+        )
     }
 }
